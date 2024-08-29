4e318916d432fd3dca93baf4ad941306e0bd21cb
rahul mali <malir5351@gmail.com>
2024-08-29 15:32:10 +0530

    get message

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port=4400;
 app.get('/getuser',(req,res)=>{
 return res.send("hello users")
 })
+app.get('/getmessage',(req,res)=>{
+    res.send({message:"second api test"})
+})
 
 app.listen(port,()=>{
     console.log(`server is runing on ${port}...`)
